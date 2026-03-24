8321ce841ed4a3d0fdb542242121c69f0d473bb5
phumlanimnguni1 <prosperproper700@gmail.com>
2026-03-24 14:09:29 +0200

    did sql AVG aggregations, filtering and sorting

--- sqlcodefile.sql
+++ sqlcodefile.sql
@@ -103,23 +103,111 @@ group by province
 
 --SECTION 3 – AVG Aggregations (10 Questions)
 --21. What is the average property price overall?
+select avg(cast(property_price as BIGINT)) as Average_Property_price
+from [property24].[dbo].[property24_v1]
+
 --22. What is the average property price per province?
+select province, avg(cast(property_price as BIGINT)) as Average_Property_price
+from [property24].[dbo].[property24_v1]
+group by province
+
 --23. What is the average property price per city?
+select city, avg(cast(property_price as BIGINT)) as Average_Property_price
+from [property24].[dbo].[property24_v1]
+group by city
+
 --24. What is the average number of bedrooms per province?
+select province, avg(cast(bedrooms as BIGINT)) as Average_Number_Bedrooms
+from [property24].[dbo].[property24_v1]
+group by province
+
 --25. What is the average number of bathrooms per province?
+select province, avg(cast(bathrooms as BIGINT)) as Average_Number_Bathrooms
+from [property24].[dbo].[property24_v1]
+group by province
+
 --26. What is the average floor size per province?
+select province, avg(cast(floor_size as BIGINT)) as Average_Floor_Size
+from [property24].[dbo].[property24_v1]
+group by province
+
 --27. What is the average monthly repayment per province?
+select province, avg(Monthly_Repayment) as Average_Monthly_Repayment
+from [property24].[dbo].[property24_v1]
+group by province
+
 --28. What is the average once-off cost per province?
+select province, avg(cast(Total_Once_off_Costs as BIGINT)) as Average_Once_Off_Costs
+from [property24].[dbo].[property24_v1]
+group by province
+
 --29. What is the average minimum gross monthly income per province?
+select province, avg(Min_Gross_Monthly_Income) as Average_Minimum_Gross_Monthly_Income
+from [property24].[dbo].[property24_v1]
+group by province
+
 --30. What is the average property price for properties above R3,000,000?
+select avg(cast(PROPERTY_PRICE as bigint)) as Average_Propery_Price
+from [property24].[dbo].[property24_v1]
+where PROPERTY_PRICE >3000000
+
 --SECTION 4 – GROUP BY + Filtering (10 Questions)
 --31. Which province has the highest average property price?
+select top 1 province, avg(cast(property_price as BIGINT)) as Average_Property_Price
+from [property24].[dbo].[property24_v1]
+group by province 
+order by Average_Property_Price desc
+
 --32. Which province has the lowest average property price?
+select top 1 province, avg(cast(property_price as BIGINT)) as Average_Property_Price
+from [property24].[dbo].[property24_v1]
+group by province 
+order by Average_Property_Price asc
+
 --33. Which city has the highest total property value?
+select top 1 city, sum(cast(property_price as BIGINT)) as Total_Property_Value
+from [property24].[dbo].[property24_v1]
+group by city 
+order by Total_Property_Value desc
+
 --34. Which city has the lowest average property price?
+select top 1 city, avg(cast(PROPERTY_PRICE as BIGINT)) as Average_Property_Price
+from [property24].[dbo].[property24_v1]
+group by city 
+order by Average_Property_Price asc
+
 --35. How many properties per province are priced above R2,000,000?
+select province, count(*) as Number_Properties
+from [property24].[dbo].[property24_v1]
+where property_price > 2000000
+group by province
+
 --36. What is the average floor size per province for properties above R3,000,000?
+select province, avg(floor_size) as Average_Floor_Size
+from [property24].[dbo].[property24_v1]
+where property_price > 3000000
+group by province
+
 --37. What is the total property value per province for properties with 3 or more bedrooms?
+select province, sum(cast(property_price as BIGINT)) as Total_Property_Value
+from [property24].[dbo].[property24_v1]
+where BEDROOMS >= 3
+group by province
+
 --38. What is the average monthly repayment per province for properties above R4,000,000?
+select province, avg(Monthly_Repayment) as Average_Monthly_Repayment
+from [property24].[dbo].[property24_v1]
+where property_price > 4000000
+group by province
+
 --39. How many properties per city have parking for 2 or more cars?
+select city, count(*) as Number_Properties
+from [property24].[dbo].[property24_v1]
+where parking >= 2
+group by city
+
 --40. What is the average minimum gross monthly income per province for properties above R5,000,000?
+select province, avg(Min_Gross_Monthly_Income) as Average_Minimum_Gross_Monthly_Income
+from [property24].[dbo].[property24_v1]
+where property_price > 5000000
+group by province
